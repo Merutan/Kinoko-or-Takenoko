cefcead203b693db6554d9d63b58bc3d205973e0
Meru <piki.0147@gmail.com>
2020-12-22 02:16:35 +0900

    今度こそ
    たけのこの里好きなのにきのこの山好きって書く屈辱

--- src/space/yurisi/Item/SpecialBook.php
+++ src/space/yurisi/Item/SpecialBook.php
@@ -9,8 +9,8 @@ use pocketmine\item\Item;
 class SpecialBook {
 	public static function get(): Item {
 		$item=Item::get(340,0);
-		$item->setCustomName("総選挙 参加賞！");
-		$item->setLore(["初めての景品付きイベント！","参加ありがとうございました！"]);
+		$item->setCustomName("総選挙 参加賞！ きのこの山への投票ありがとうございました！");
+		$item->setLore(["初めての景品付きイベント！","きのこの山しか勝たん！参加ありがとうございました！"]);
 		return $item;
 	}
 
